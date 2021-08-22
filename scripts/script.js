674a3b374fca350191033dbdc3865c6b10902a2f
Anjel <Whie1@yandex.ru>
2021-08-22 23:27:36 +0200

    Проектная работа 6.2

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,8 @@ const popupAddFormUrlInput = popupAddCard.querySelector('.popup__input_add_link'
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popups = document.querySelectorAll('.popup')
+const popupAddForm = document.querySelector('.popup__form-add');
+const buttonElement = popupAddForm.querySelector('.popup__button');
 
 
 const initialCards = [
@@ -81,13 +83,11 @@ function closeByEscape(evt) {
 function openPopup(popup) {
   popup.classList.add('popup_opened');
   document.addEventListener('keydown', closeByEscape);
-
-  popup.querySelector(`.${popup.id}-overlay`).addEventListener('click', () => closePopup(popup));
 }
 
 popups.forEach((popup) => {
   popup.addEventListener('click', (evt) => {
-      if (evt.target.classList.contains('popup_opened')) {
+      if (evt.target.classList.contains('popup__overlay')) {
           closePopup(popup);
       }
       if (evt.target.classList.contains('popup__close-icon')) {
@@ -121,12 +121,19 @@ function sumbitFormEditProfile(evt) {
   closePopup(popupEditProfile);
 }
 
+function disableButton() {
+  buttonElement.setAttribute("disabled", true);
+  buttonElement.classList.add('popup__button_disabled');
+}
+
 function handleFormAddCard(evt) {
   evt.preventDefault();
   const name = evt.target.querySelector('.popup__input_add_title').value;
   const link = evt.target.querySelector('.popup__input_add_link').value;
   const cardElement = createCard({ name, link })
   elementContainer.prepend(cardElement);
+  popupAddForm.reset();
+  disableButton();
   closePopup(popupAddCard);
 }
 
